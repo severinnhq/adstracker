8e12f4c60b443c8f354eef13ae1282415953d43c
severinn <severinnhq@gmail.com>
2026-03-21 16:44:55 +0100

    redi

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, MouseEvent } from "react";
 import { Campaign, NICHES, Niche } from "@/types";
 import { getCampaigns, saveCampaigns, deleteCampaign } from "@/lib/storage";
